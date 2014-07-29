ca72d368d4cefa341ebf49b7161015591e6af743
Jesus Magana <maganazero5@gmail.com>
2014-07-29 16:26:45 -0700

    Finished logic to print out

--- serviceLane.cpp
+++ serviceLane.cpp
@@ -21,8 +21,19 @@ int main()
 		mystring = mystring.substr(breakNums+1);		
 	}
 	
-	for(int i = 0; i < size; i++){
-		cout << lanes[i] << endl;
+	for(int i = 0; i < testNum; i++){
+		getline(cin, mystring);
+		breakPt = mystring.find(" ");
+		int begin = atoi(mystring.substr(0,breakPt).c_str());
+		int end = atoi(mystring.substr(breakPt+1).c_str());
+		
+		int minimumSize = 3;
+		for(int j = begin; j <= end; j++){
+			if (minimumSize > lanes[j]){
+				minimumSize = lanes[j];
+			}
+		}		
+		cout << minimumSize << endl;
 	}
 
 	return 0;

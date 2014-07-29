5af25d0339f5efa75795eefe77085f48f9d2cc50
Jesus Magana <maganazero5@gmail.com>
2014-07-29 16:08:41 -0700

    Have logic for reading in a line

--- serviceLane.cpp
+++ serviceLane.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello" << endl;
+	string mystring;
+	getline(cin, mystring);
+	size_t breakPt = mystring.find(" ");
+	int size = atoi(mystring.substr(0,breakPt).c_str());
+	int testNum = atoi(mystring.substr(breakPt+1).c_str());
 
+	cout << "Size:" << size << " testNum:" << testNum << endl;	
 	return 0;
 }

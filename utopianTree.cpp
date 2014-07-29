a0e798eb2d08a1161771fec7418fd66c571a3136
Jesus Magana <maganazero5@gmail.com>
2014-07-29 16:31:27 -0700

    Starter code for utopian tree

---
+++ utopianTree.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int height(int n) {
+    return 0;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << height(n) << endl;
+    }
+}

cead77ff12f621557b14fae48de41028f87bd9da
Jesus Magana <maganazero5@gmail.com>
2014-07-29 16:16:59 -0700

    Take second line and break into ints

--- serviceLane.cpp
+++ serviceLane.cpp
@@ -12,6 +12,18 @@ int main()
 	int size = atoi(mystring.substr(0,breakPt).c_str());
 	int testNum = atoi(mystring.substr(breakPt+1).c_str());
 
-	cout << "Size:" << size << " testNum:" << testNum << endl;	
+	int lanes[size];
+
+	getline(cin, mystring);
+	for(int i = 0; i < size; i++){
+		size_t breakNums = mystring.find(" ");
+		lanes[i] = atoi(mystring.substr(0,breakNums).c_str());
+		mystring = mystring.substr(breakNums+1);		
+	}
+	
+	for(int i = 0; i < size; i++){
+		cout << lanes[i] << endl;
+	}
+
 	return 0;
 }

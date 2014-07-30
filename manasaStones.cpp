35b1f3a7204b1002812bbad3085fcf06aa6014f3
Jesus Magana <maganazero5@gmail.com>
2014-07-30 15:42:59 -0700

    Used editorial pseudocode as basis to realize pascals triangle

--- manasaStones.cpp
+++ manasaStones.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <math.h>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main()
@@ -19,39 +17,24 @@ int main()
 
 		int b;
 		cin >> b;
-
-		//set up the vector and add the base cases
-		vector<int> v;
-
-		v.push_back(0);
-		v.push_back(0);
-
 		//logic
-		for(int i = 1; i < rockNum;i++){
-			for(int n = 1; n <= pow(2, i); n++){
-				int newInt = v.front();
-				v.push_back(newInt + a);
-				v.push_back(newInt + b);
-				v.erase(v.begin());
-			}
+		if(a == b){
+			cout << a * (rockNum - 1) << endl;
+			continue;
 		}
 
-		
-		for(int i = 0;i < v.size(); i++){
-			int lastPosition = 0;
-			for(int j = i+1; j < v.size();j++){
-				if(v.at(j) == v.at(i)){
-					lastPosition = max(j - 2, i);
-					v.erase(v.begin() + j);
-					j = lastPosition;
-				}
+		if(a < b){
+			int t = rockNum - 1;
+			for(int j = 0; j < rockNum; j++, t--){
+				cout << t*a + j*b << " ";
 			}
-		}
-
-		for(int i = 0; i < v.size(); i++){
-			cout << v.at(i) << " ";
-		}
-		cout << endl;
+			cout << endl;
+		} else {
+			int t = rockNum - 1;
+			for(int j = 0; j < rockNum; j++, t--){
+				cout << t*b + j*a << " ";
+			}
+			cout << endl;		}
 	}
 
 	return 0;

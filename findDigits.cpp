9feb5349f5d69efe78dd97a1cdc6be2acb8da65b
Jesus Magana <maganazero5@gmail.com>
2014-07-30 16:06:39 -0700

    Solved find digits via mod function and division

--- findDigits.cpp
+++ findDigits.cpp
@@ -4,10 +4,25 @@ using namespace std;
 
 int main()
 {
-	int num;
-	cin >> num;
+	int count;
+	cin >> count;
 
-	cout << num << endl;
+	for(int i = 0; i < count; i++){
+		unsigned long num;
+		cin >> num;
+		int result = 0;
+		int modingNum = num;
+		while(modingNum != 0){
+			int subNum = modingNum % 10;
+			if(subNum != 0){
+				if (num % subNum == 0){
+					result++;
+				}
+			}
+			modingNum /= 10;
+		}
+		cout << result << endl;
+	}
 
 	return 0;
 }

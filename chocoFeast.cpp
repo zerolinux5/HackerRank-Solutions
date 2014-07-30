de1b6371d78a7210750242ec37a58825e79e4f8e
Jesus Magana <maganazero5@gmail.com>
2014-07-30 11:41:11 -0700

    Starting code for chocolate feast

---
+++ chocoFeast.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int t,n,c,m;
+    cin>>t;
+    while(t--){
+        cin>>n>>c>>m;
+        int answer=0;
+        // Computer answer
+        
+        cout<<answer<<endl;
+    }
+    return 0;
+}

85b5648f0522b5c029fa0e0a3fb42454b45a9d6a
Jesus Magana <maganazero5@gmail.com>
2014-07-30 10:53:18 -0700

    Starting solution

--- got.cpp
+++ got.cpp
@@ -1,13 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-
+#include <algorithm>
+#include <string>
 using namespace std;
 
-int main()
-{
-	string stringCheck;
-	cin >> stringCheck;
 
-	cout << stringCheck << endl;
-
-	return 0;
+int main() {
+   
+    string s;
+    cin>>s;
+     
+    int flag = 0;
+    // Assign Flag a value of 0 or 1 depending on whether or not you find what you are looking for, in the given string 
+    if(flag==0)
+        cout<<"NO";
+    else
+        cout<<"YES";
+    return 0;
 }

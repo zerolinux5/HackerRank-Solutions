466b794e656a39cc930db5e7f1ce0aa0c44a4cc8
Jesus Magana <maganazero5@gmail.com>
2014-07-30 09:43:52 -0700

    Finished logic using ascii

--- loveLetter.cpp
+++ loveLetter.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -7,6 +9,49 @@ int main()
 	int num;
 	cin >> num;
 
-	cout << num << endl;
+	for(int i = 0; i < num ;i++){
+		string word;
+		cin >> word;
+
+		int j = 0;
+		int t = word.size() - 1;
+
+		int numOps = 0;
+
+		for(; j < (word.size() / 2);j++, t--){
+			int beginStr = (int)word.at(j);
+			int endStr = (int)word.at(t);
+
+			while(beginStr != endStr){
+				if(endStr == 97 && beginStr == 97){
+					break;
+				}
+				if(beginStr < endStr){
+					endStr--;
+					numOps++;
+				} else {
+					beginStr--;
+					numOps++;
+				}
+			}
+			/*
+			//create the char, string, and sstream to convert char to string for fcn
+			char newChar = endStr;
+			stringstream ss;
+			string newString;
+
+			//put char in sstream
+			ss << newChar;
+			ss >> newString;
+
+
+			int location = t;
+			word.replace(location, 1, newString);
+			*/
+		}
+
+		cout << numOps << endl;
+	}
+
 	return 0;
 }

2096f34e9516a69ba56a505bdd628654456fc2b0
Jesus Magana <maganazero5@gmail.com>
2014-07-30 12:10:35 -0700

    Solved logic using division and mod

--- chocoFeast.cpp
+++ chocoFeast.cpp
@@ -1,10 +1,20 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
+int wrapperCount(int tradePrice, int wrappers){
+    int newChoco = 0;
+    if(wrappers >= tradePrice){
+        int newWrappers = wrappers / tradePrice;
+        wrappers %= tradePrice;
+        wrappers += newWrappers;
+        newChoco = newWrappers;
+        if(wrappers >= tradePrice){
+            return newChoco + wrapperCount(tradePrice, wrappers);
+        }
+    }
+
+    return newChoco;
+}
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
@@ -14,7 +24,13 @@ int main() {
         cin>>n>>c>>m;
         int answer=0;
         // Computer answer
+        int chocoCount = n / c;
+        int wrappers = chocoCount;
+
+        chocoCount += wrapperCount(m, wrappers);
         
+        answer = chocoCount;
+
         cout<<answer<<endl;
     }
     return 0;

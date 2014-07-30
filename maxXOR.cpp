06b019571b884c112343c6f22c1c2bf287b05f74
Jesus Magana <maganazero5@gmail.com>
2014-07-30 11:15:55 -0700

    Starting max xor challenge

---
+++ maxXOR.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+ * Complete the function below.
+ */
+int maxXor(int l, int r) {
+
+
+}
+
+int main() {
+    int res;
+    int _l;
+    cin >> _l;
+    
+    int _r;
+    cin >> _r;
+    
+    res = maxXor(_l, _r);
+    cout << res;
+    
+    return 0;
+}

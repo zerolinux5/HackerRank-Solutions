a04af02b8583981ea2e7f3692f853bd8eb0c3aaa
Jesus Magana <maganazero5@gmail.com>
2014-07-30 16:11:57 -0700

    Starting filling jars solution

---
+++ fillingJars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numJars, numOps;
+	cin >> numJars;
+	cin >> numOps;
+
+	cout << "Jars:" << numJars << " Ops:" << numOps << endl;
+	return 0;
+}

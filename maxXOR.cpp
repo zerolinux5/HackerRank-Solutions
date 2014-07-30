50d6ea81cc3c7b527082ffdaf6db9b134bc7ee78
Jesus Magana <maganazero5@gmail.com>
2014-07-30 11:29:03 -0700

    Finished logic with 2 loops

--- maxXOR.cpp
+++ maxXOR.cpp
@@ -1,27 +1,19 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <cstdlib>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 /*
  * Complete the function below.
  */
 int maxXor(int l, int r) {
+	int maxX = 0;
+	for(int i = l; i <= r; i++){
+		for(int j = l; j < r; j++){
+			if ((j ^ i) > maxX){
+				maxX = (j ^ i);
+			} 
+		}
+	}
 
-
+	return maxX;
 }
 
 int main() {

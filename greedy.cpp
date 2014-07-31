61d8d82a74d75f5bd41a887e23edcaeac599e957
Jesus Magana <maganazero5@gmail.com>
2014-07-31 14:21:13 -0700

    Solved via the greedy solution of cheapest toys

--- greedy.cpp
+++ greedy.cpp
@@ -5,10 +5,29 @@ using namespace std;
 
 int main()
 {
-	int numToys, money;
+	int numToys, money, numBought, i;
 	cin >> numToys >> money;
 
-	cout << numToys << " " << money << endl;
+	long toyPrices[numToys];
+	numBought = 0;
+
+	for(i = 0; i < numToys;i++){
+		cin >> toyPrices[i];
+	}
+
+	sort(toyPrices, toyPrices + numToys);
+	i = 0;
+
+	while(1){
+		if(money - toyPrices[i] < 0){
+			break;
+		}
+		money -= toyPrices[i];
+		numBought++;
+		i++;
+	}
+
+	cout << numBought << endl;
 
 	return 0;
 }

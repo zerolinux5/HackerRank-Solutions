78ebf12d099981272e8ca306e37d8969e9d8225a
Jesus Magana <maganazero5@gmail.com>
2014-07-31 09:33:41 -0700

    Starting angry children

---
+++ angryChildren.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+// It is NOT mandatory to use the provided template. You can handle the IO section differently.
+
+int main() {
+    /* The code required to enter n,k, candies is provided*/
+
+    int N, K, unfairness;
+    cin >> N >> K;
+    int candies[N];
+    for (int i=0; i<N; i++)
+        cin >> candies[i];
+    
+    /** Write the solution code here. Compute the result, store in  the variable unfairness --
+    and output it**/
+    cout << unfairness << "\n";
+    return 0;
+}
--- sherlockGCD.cpp
+++ sherlockGCD.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -8,7 +9,39 @@ int main()
 	cin >> testNum;
 
 	for(int i = 0; i < testNum; i++){
+		int numRead;
+		cin >> numRead;
+		int flag = 0;
 
+		int numArray[numRead];
+		for(int j = 0; j < numRead; j++){
+			cin >> numArray[j];
+		}
+
+		for(int j = 0; j < numRead; j++){
+			for(int k = j+1; k < numRead; k++){
+				int divisible = 2;
+				while(divisible <= max(numArray[k], numArray[j])){
+					if((numArray[k] % divisible != 0) && (numArray[j] % divisible != 0)){
+						cout << "YES" << endl;
+						flag = 1;
+						break;
+					}
+					divisible++;
+				}
+
+				if(flag){
+					break;
+				}
+			}
+
+			if(flag){
+				break;
+			}
+		}
+		if(!flag){
+			cout << "NO" << endl;
+		}
 	}
 
 	return 0;

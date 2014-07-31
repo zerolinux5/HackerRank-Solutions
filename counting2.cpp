1c43c7c45a1f6eeda565528a4535f3660aea2bde
Jesus Magana <maganazero5@gmail.com>
2014-07-31 14:10:41 -0700

    Sort via running through array and displaying ints

---
+++ counting2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numInputs;
+	cin >> numInputs;
+
+	int array[100];
+
+	for(int i = 0; i < 100; i++){
+		array[i] = 0;
+	}
+
+	while(numInputs--){
+		int newNum;
+		cin >> newNum;
+		array[newNum]++;
+	}
+
+	for(int i = 0; i < 100;i++){
+		if(array[i] > 0){
+			while(array[i]--){
+				cout << i << " ";
+			}
+		}
+	}
+	cout << endl;
+}

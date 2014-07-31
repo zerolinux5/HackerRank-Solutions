9efff12261235eda1c56b4035a136280e5ed3420
Jesus Magana <maganazero5@gmail.com>
2014-07-31 16:18:24 -0700

    Basic run through with if statements to check if move number

--- basicInsert.cpp
+++ basicInsert.cpp
@@ -7,13 +7,45 @@ int main()
 	int arraySize;
 	cin >> arraySize;
 	int array[arraySize];
-	int location = arraySize -1;
+	int location = arraySize - 1;
 
 	for(int i = 0; i < arraySize;i++){
 		cin >> array[i];
 	}
 
+	int shiftNum = array[arraySize - 1];
+	while(location){
+		if(shiftNum < array[location - 1]){
+			array[location] = array[location-1];
+		} else if(shiftNum == array[location-1]){
+			array[location] = shiftNum;
+			for(int i = 0; i < arraySize;i++){
+				cout << array[i] << " ";
+			}
+			cout << endl;
+			break;
+		} else {
+			array[location] = shiftNum;
+			for(int i = 0; i < arraySize;i++){
+				cout << array[i] << " ";
+			}
+			cout << endl;
+			break;
+		}
+		location--;
+		for(int i = 0; i < arraySize;i++){
+			cout << array[i] << " ";
+		}
+		cout << endl;
+	}
 
+	if(shiftNum < array[location]){
+		array[0] = shiftNum;
+		for(int i = 0; i < arraySize;i++){
+			cout << array[i] << " ";
+		}
+		cout << endl;
+	}
 
 	return 0;
 }

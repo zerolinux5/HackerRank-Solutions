7d23be55afd85659fa5ba7c7507a7aa35c126c38
Jesus Magana <maganazero5@gmail.com>
2014-07-31 11:22:41 -0700

    Starting sherlock queries

---
+++ sherlockQueries.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+	unsigned long arrayA[N];
+	unsigned long arrayB[M];
+	unsigned long arrayC[M];
+
+	
+	cout << endl;
+	return 0;
+}

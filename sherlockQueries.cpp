ef856dadb10562db33f01714c45325d9d90d3c82
Jesus Magana <maganazero5@gmail.com>
2014-07-31 12:30:01 -0700

    Given code for insertion sort(wrong)

--- sherlockQueries.cpp
+++ sherlockQueries.cpp
@@ -10,7 +10,30 @@ int main()
 	unsigned long arrayB[M];
 	unsigned long arrayC[M];
 
-	
+	for(int i = 0; i < N; i++){
+		cin >> arrayA[i];
+	}
+
+	for(int i = 0; i < M; i++){
+		cin >> arrayB[i];
+	}
+
+	for(int i = 0; i < M; i++){
+		cin >> arrayC[i];
+	}	
+
+	for(int i = 0; i < M; i++){
+		for(int j = 0; j < N; j++){
+			if((j+1) % arrayB[i] == 0){
+				arrayA[j] = arrayA[j] * arrayC[i];
+			}
+		}
+	}
+
+	for(int i = 0; i < N; i++){
+		unsigned long moddedResult = (arrayA[i] % 1000000007);
+		cout << moddedResult << " ";
+	}
 	cout << endl;
 	return 0;
 }

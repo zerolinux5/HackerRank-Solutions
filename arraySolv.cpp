d2595b537d6f4da60ac1a2945a73177c81cb2b67
Jesus Magana <maganazero5@gmail.com>
2014-07-31 14:46:18 -0700

    Solved via sort ascending and descending and comparing

--- arraySolv.cpp
+++ arraySolv.cpp
@@ -3,17 +3,43 @@
 
 using namespace std;
 
-bool myfunction (int i,int j) { return (i>j); }
+bool myfunction (unsigned long i,unsigned long j) { return (i>j); }
 
 int main()
 {
-	int testNum, size, value, flag = 1;
+	unsigned long testNum, size, value, flag;
 	cin >> testNum;
+
+	while(testNum--){
+		flag = 1;
+		cin >> size >> value;
+
+		unsigned long arrayA[size];
+		unsigned long arrayB[size];
+
+		for(int i = 0; i < size; i++){
+			cin >> arrayA[i]; 
+		}
+
+		for(int i = 0; i < size; i++){
+			cin >> arrayB[i]; 
+		}
+
+		sort(arrayA, arrayA + size);
+		sort(arrayB, arrayB + size, myfunction);
+
+		for(int i = 0; i < size;i++){
+			if(arrayA[i] + arrayB[i] < value){
+				flag = 0;
+				break;
+			}
+		}
 
 		if (flag){
 			cout << "YES" << endl;
 		} else {
 			cout << "NO" << endl;
 		}
+	}
 	return 0;
 }

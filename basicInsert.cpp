e91980a3586ec141efb42e566baf82201884ccc1
Jesus Magana <maganazero5@gmail.com>
2014-07-31 16:00:12 -0700

    starting algorithm for inserting

---
+++ basicInsert.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arraySize;
+	cin >> arraySize;
+	int array[arraySize];
+	int location = arraySize -1;
+
+	for(int i = 0; i < arraySize;i++){
+		cin >> array[i];
+	}
+
+
+
+	return 0;
+}

e42ba8666bbf958f62c812d3f262858b404ea2bc
Jesus Magana <maganazero5@gmail.com>
2014-07-30 17:15:52 -0700

    Solved via multiplication

--- fillingJars.cpp
+++ fillingJars.cpp
@@ -7,7 +7,19 @@ int main()
 	int numJars, numOps;
 	cin >> numJars;
 	cin >> numOps;
+	unsigned long result = 0;
 
-	cout << "Jars:" << numJars << " Ops:" << numOps << endl;
+	for(int i = 0; i < numOps; i++){
+		unsigned long jar1, jar2, amount;
+		cin >> jar1;
+		cin >> jar2;
+		cin >> amount;
+		
+		result += (amount * (jar2 - jar1 + 1));
+	}
+
+	result /= numJars; 
+	cout << result << endl;
+
 	return 0;
 }

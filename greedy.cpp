197d9bc909d3c45182bb2e285cd5f45574a7607f
Jesus Magana <maganazero5@gmail.com>
2014-07-31 14:14:22 -0700

    Looking for most toys using set cash

---
+++ greedy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int numToys, money;
+	cin >> numToys >> money;
+
+	cout << numToys << " " << money << endl;
+
+	return 0;
+}

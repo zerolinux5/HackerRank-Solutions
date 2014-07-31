462e98f20bf8cc0c7eab08755a3bf7458cbf502e
Jesus Magana <maganazero5@gmail.com>
2014-07-31 16:47:30 -0700

    given algorithm

---
+++ partitionSort.cpp
@@ -0,0 +1,35 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void quickSort(vector <int>  ar, int ar_size) {
+
+
+}
+int main(void) {
+   vector <int>  _ar;
+   int _ar_size;
+cin >> _ar_size;
+for(int _ar_i=0; _ar_i<_ar_size; _ar_i++) {
+   int _ar_tmp;
+   cin >> _ar_tmp;
+   _ar.push_back(_ar_tmp); 
+}
+
+quickSort(_ar, _ar_size);
+   
+   return 0;
+}

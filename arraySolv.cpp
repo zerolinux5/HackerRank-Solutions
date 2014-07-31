bbef0ca43b188f32ea8ffb48c0de4ad6ad58d9da
Jesus Magana <maganazero5@gmail.com>
2014-07-31 14:38:19 -0700

    Starting the solution for array index comparison

---
+++ arraySolv.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool myfunction (int i,int j) { return (i>j); }
+
+int main()
+{
+	int testNum, size, value, flag = 1;
+	cin >> testNum;
+
+		if (flag){
+			cout << "YES" << endl;
+		} else {
+			cout << "NO" << endl;
+		}
+	return 0;
+}

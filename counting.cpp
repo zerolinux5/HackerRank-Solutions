66775443b7ecfd9903e0056424f995faa4ffe5e7
Jesus Magana <maganazero5@gmail.com>
2014-07-31 14:04:43 -0700

    Take number input and add 1 to its location in array

--- counting.cpp
+++ counting.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main()
 {
-	
+	int numInputs;
+	cin >> numInputs;
+
+	int array[100];
+
+	for(int i = 0; i < 100; i++){
+		array[i] = 0;
+	}
+
+	while(numInputs--){
+		int newNum;
+		cin >> newNum;
+		array[newNum]++;
+	}
+
+	for(int i = 0; i < 100;i++){
+		cout << array[i] << " ";
+	}
+	cout << endl;
 }

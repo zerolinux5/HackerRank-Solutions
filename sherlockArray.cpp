782f23c7a5a658ba47514b05acd503b5a873402f
Jesus Magana <maganazero5@gmail.com>
2014-07-31 15:23:05 -0700

    solved via adding both sided around pivot

--- sherlockArray.cpp
+++ sherlockArray.cpp
@@ -8,7 +8,35 @@ int main()
 	cin >> testNum;
 
 	while(testNum--){
+		int size;
+		cin >> size;
+		int array[size];
+		int flag = 0;
+		for(int i = 0; i < size;i++){
+			cin >> array[i];
+		}
 
+		int location = 0;
+		int rightSide = 0;
+		int leftSide = 0; 
+		int switchNum;
+		for(int i = 1; i < size; i++){
+			rightSide+= array[i];
+		}
+
+		while(location < size){
+			if(rightSide == leftSide){
+				cout << "YES" << endl;
+				flag = 1;
+				break;
+			}
+			rightSide -= array[location+1];
+			leftSide += array[location];
+			location++;
+		}
+		if(!flag){
+			cout << "NO" << endl;
+		}
 	}
 
 	return 0;

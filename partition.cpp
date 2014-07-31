27b5d37dc0a8450070a6590486b4bd233dfb9b10
Jesus Magana <maganazero5@gmail.com>
2014-07-31 16:44:18 -0700

    solved via pivots and array

--- partition.cpp
+++ partition.cpp
@@ -6,9 +6,38 @@ int main()
 {
 	int size;
 	cin >> size;
+	int biggerCount = 0;
+	int pivot;
+	int array[size];
 
 	for(int i = 0; i < size;i++){
-	
+		cin >> array[i];
+		if(i == 0){
+			pivot = array[i];
+		}
+		if(array[i] > pivot){
+			biggerCount++;
+		}
 	}
+
+	int finalArray[size];
+	int rightEnd = size - biggerCount;
+	int leftEnd = 0;
+	finalArray[rightEnd-1] = pivot;
+	for(int i = 1; i < size;i++){
+		if(array[i] > pivot){
+			finalArray[rightEnd] = array[i];
+			rightEnd++;
+		} else {
+			finalArray[leftEnd] = array[i];
+			leftEnd++;			
+		}
+	}
+
+	for(int i = 0; i < size;i++){
+		cout << finalArray[i] << " ";
+	}
+	cout << endl;
+
 	return 0;
 }

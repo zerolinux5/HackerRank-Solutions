f31fd91b40b7ae916043a14316612e6dbe8970ea
Jesus Magana <maganazero5@gmail.com>
2014-07-31 13:43:27 -0700

    Solved by making into array and sorting

--- countingSort.cpp
+++ countingSort.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -7,7 +7,33 @@ int main()
 	int testNum;
 	cin >> testNum;
 
-	cout << testNum << endl;
+	int array[testNum];
+	int finalArray[100];
+	int finalNum = 0;
 
+	for(int i = 0; i < testNum; i++){
+		cin >> array[i];
+
+		string ignore;
+		cin >> ignore;
+	}
+
+	sort(array, array + testNum);
+
+	int continuous = 0;
+	for(int i = 0; i < testNum; i++){
+		finalNum++;
+		finalArray[(array[i])] = finalNum;
+	}
+
+	for(int i = 0; i<100; i++){
+		if(i >= array[testNum-1]){
+			cout << finalNum << " ";
+		} else {
+			cout << finalArray[i] << " ";
+		}
+	}
+
+	cout << endl;
 	return 0;
 }

07ecf421743248c54d25b330c754110e52c6f543
Jesus Magana <maganazero5@gmail.com>
2014-08-01 20:14:42 -0700

    Solved with looping through sorted array

--- closestNum.cpp
+++ closestNum.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	int testNum;
-	cin >> testNum;
-	while(testNum--){
+	int numSize;
+	cin >> numSize;
+	long nums[numSize] ;
 
+	for(int i = 0; i < numSize; i++){
+		cin >> nums[i];
 	}
 
+	sort(nums, nums + numSize);
+	long minNum = nums[numSize - 1] - nums[0];
+	for(int i = 0; i < (numSize - 1); i++){
+		long currentDiff = nums[i+1] - nums[i];
+		if(currentDiff < minNum){
+			minNum = currentDiff;
+		}
+	}
+
+	for(int i = 0; i < (numSize - 1); i++){
+		long currentDiff = nums[i+1] - nums[i];
+		if(currentDiff == minNum){
+			cout << nums[i] << " " << nums[i+1] << " ";
+		}
+	}
+
+	cout << endl;
+	
 	return 0;
 }

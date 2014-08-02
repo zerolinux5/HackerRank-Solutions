f204cc532d0fefa7194fce7653b5dca9fd074961
Jesus Magana <maganazero5@gmail.com>
2014-08-02 12:08:02 -0700

    Solved using cases for min sizes and looping

--- encryptionMatrix.cpp
+++ encryptionMatrix.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <math.h>
 
 using namespace std;
 
@@ -7,7 +9,97 @@ int main()
 	string inputString;
 	cin >> inputString;
 
-	cout << inputString << endl;
+	int min = floor(sqrt(inputString.size()));
+	int max = ceil(sqrt(inputString.size()));
+
+	if(pow(min, 2) >= inputString.size()){
+		char wordMatrix[min][min];
+		int count = 0;
+		for(int i = 0; i < min; i++){
+			for(int j = 0; j < min;j++){
+				if( count >= inputString.size()){
+					inputString += " ";
+				}
+				wordMatrix[i][j] = inputString.at(count);
+				count++;
+			}
+		}
+
+		count = 0;
+		for(int j = 0; j < min; j++){
+			//if(flag){
+				for(int i = 0; i < min; i++){
+					if( (int)(wordMatrix[i][j]) == 32){
+						continue;
+					}
+					cout << wordMatrix[i][j];
+					//count++;
+				}
+				cout << " ";
+			//} else {
+			//	break;
+			//}
+		}
+
+	} else if ( (min * max) >= inputString.size()){
+		char wordMatrix[min][max];
+		int count = 0;
+		for(int i = 0; i < min; i++){
+			for(int j = 0; j < max;j++){
+				if( count >= inputString.size()){
+					inputString += " ";
+				}
+				wordMatrix[i][j] = inputString.at(count);
+				count++;
+			}
+		}
+
+		count = 0;
+		for(int j = 0; j < max; j++){
+			//if(flag){
+				for(int i = 0; i < min; i++){
+					if( (int)(wordMatrix[i][j]) == 32){
+						continue;
+					}
+					cout << wordMatrix[i][j];
+					//count++;
+				}
+				cout << " ";
+			//} else {
+			//	break;
+			//}
+		}
+	} else {
+		char wordMatrix[max][max];
+		int count = 0;
+		for(int i = 0; i < max; i++){
+			for(int j = 0; j < max;j++){
+				if( count >= inputString.size()){
+					inputString += " ";
+				}
+				wordMatrix[i][j] = inputString.at(count);
+				count++;
+			}
+		}
+
+		count = 0;
+		for(int j = 0; j < max; j++){
+			//if(flag){
+				for(int i = 0; i < max; i++){
+					if( (int)(wordMatrix[i][j]) == 32){
+						continue;
+					}
+					cout << wordMatrix[i][j];
+					//count++;
+				}
+				cout << " ";
+			//} else {
+			//	break;
+			//}
+		}
+	}
+
+	cout << endl;
 
 	return 0;
 }

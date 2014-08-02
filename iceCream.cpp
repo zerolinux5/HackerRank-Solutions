8b4cebd816ed6e87d733b435bc35ac09402032e3
Jesus Magana <maganazero5@gmail.com>
2014-08-01 20:04:00 -0700

    Solved using loops

--- iceCream.cpp
+++ iceCream.cpp
@@ -7,5 +7,34 @@ int main()
 	int testNum;
 	cin >> testNum;
 
+	while(testNum--){
+		int flag = 1;
+
+		int wantPrice;
+		cin >> wantPrice;
+
+		int prices;
+		cin >> prices;
+
+		int iceCreams[prices];
+
+		for(int i = 0; i < prices; i++){
+			cin >> iceCreams[i];
+		}
+		for(int i = 0; i < prices; i++){
+			if(flag){
+				for(int j = i+1; j < prices;j++){
+					if((iceCreams[i] + iceCreams[j]) == wantPrice){
+						cout << (i+1) << " " << (j + 1) << endl;
+						flag = 0;
+						break;
+					}
+				}
+			} else {
+				break;
+			}
+		}
+	}
+
 	return 0;
 }

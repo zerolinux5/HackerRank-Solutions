f97b4685dfddca9d743ac91fbadfbe15d80f2b1d
Jesus Magana <maganazero5@gmail.com>
2014-08-01 20:29:54 -0700

    Starting code to find median

---
+++ median.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arraySize;
+	cin >> arraySize;
+
+	int smaller = 0;
+	int bigger = 0;
+	for(int i = 0; i < arraySize;i++){
+		int newNum;
+		cin >> newNum;
+		
+	}
+
+	return 0;
+}

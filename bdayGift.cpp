bcb5c4f0a51c021f55c7265be47d5b4d773da8ca
Jesus Magana <maganazero5@gmail.com>
2014-08-03 15:38:01 -0700

    Works but test won't read it

--- bdayGift.cpp
+++ bdayGift.cpp
@@ -6,10 +6,18 @@ int main()
 {
 	int numTimes;
 	cin >> numTimes;
+	double result = 0;
 
 	while(numTimes--){
-
+		double newNum;
+		cin >> newNum;
+		result += (0.5 * newNum);
 	}
+
+	cout.precision(1);
+	cout << fixed;
+
+	cout << result << endl;
 
 	return 1;
 }

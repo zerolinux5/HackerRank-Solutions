2772a8b09c0169f88f1b161b962f4ad96ef6f1db
Jesus Magana <maganazero5@gmail.com>
2014-08-03 15:54:45 -0700

    solved via symmetry point equation

--- findPoint.cpp
+++ findPoint.cpp
@@ -8,7 +8,12 @@ int main()
 	cin >> testNum;
 
 	while(testNum--){
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
 
+		int pointSX = 2*x2 - x1;
+		int pointSY = 2*y2 - y1;
+		cout << pointSX << " " << pointSY << endl;
 	}
 
 	return 0;

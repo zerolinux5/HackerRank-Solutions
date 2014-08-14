4c98a6c697a435968f69bd97748663dd2ec0cae5
Jesus Magana <maganazero5@gmail.com>
2014-08-13 21:51:30 -0700

    Solved via multiplication

--- townConnection.cpp
+++ townConnection.cpp
@@ -2,11 +2,23 @@
 
 using namespace std;
 
+
 int main(){
 	int testNum;
 	cin >> testNum;
 
 	while(testNum--){
+		int num;
+		cin >> num;
+		int result = 1;
+		for(int i = 0; i < (num - 1);i++){
+			int newNum;
+			cin >> newNum;
+			result *= newNum;
+			result %= 1234567;
+		}
+
+		cout << result << endl;
 
 	}
 	return 0;

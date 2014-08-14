7ead1c224f08897bc19eb2798c4b2a072b795965
Jesus Magana <maganazero5@gmail.com>
2014-08-13 18:00:57 -0700

    Worked a bit on sherlock queries

--- sherlockQueries.cpp
+++ sherlockQueries.cpp
@@ -25,14 +25,13 @@ int main()
 	for(int i = 0; i < M; i++){
 		for(int j = 0; j < N; j++){
 			if((j+1) % arrayB[i] == 0){
-				arrayA[j] = arrayA[j] * arrayC[i];
+				arrayA[j] = ((arrayA[j] * arrayC[i]) % 1000000007);
 			}
 		}
 	}
 
 	for(int i = 0; i < N; i++){
-		unsigned long moddedResult = (arrayA[i] % 1000000007);
-		cout << moddedResult << " ";
+		cout << arrayA[i] << " ";
 	}
 	cout << endl;
 	return 0;

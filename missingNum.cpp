c61a1c99a696c2abc8d12a4d09b2d9dc708d703d
Jesus Magana <maganazero5@gmail.com>
2014-08-14 18:02:26 -0700

    Sort the two lists

--- missingNum.cpp
+++ missingNum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -6,7 +7,27 @@ int main()
 {
 	int sizeA,sizeB;
 	cin >> sizeA;
+	int arrayA[sizeA];
+	for(int i = 0; i < sizeA; i++){
+		cin >> arrayA[i];
+	}
+
+	cin >> sizeB;
+	int arrayB[sizeB];
+	for(int i = 0; i < sizeB; i++){
+		cin >> arrayB[i];
+	}
+
+	sort(arrayA, arrayA + sizeA);
+	for(int i = 0; i < sizeA;i++){
+		cout << arrayA[i] << endl;
+	}
+
+	sort(arrayB, arrayB + sizeB);
+	for(int i = 0; i < sizeB;i++){
+		cout << arrayB[i] << endl;
+	}
 
 	return 0;
-	
+
 }

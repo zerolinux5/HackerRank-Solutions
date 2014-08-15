3894c1cb9772a37e9dcdb250a261ca70390235d7
Jesus Magana <maganazero5@gmail.com>
2014-08-14 17:19:08 -0700

    Solved using powers of 2

--- lights.cpp
+++ lights.cpp
@@ -8,7 +8,17 @@ int main()
 	cin >> testNum;
 
 	while(testNum--){
+		int num;
+		cin >> num;
 
+		int result = 2;
+		num--;
+		while(num--){
+			result *= 2;
+			result %= 100000;
+		}
+		result--;
+		cout << result << endl;
 	}
 	return 0;
 }

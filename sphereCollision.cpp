572790bc32e5c628e26a9a694f553cf8ca079e88
Jesus Magana <maganazero5@gmail.com>
2014-08-16 18:46:07 -0700

    Made spheres and input into them

--- sphereCollision.cpp
+++ sphereCollision.cpp
@@ -2,12 +2,32 @@
 
 using namespace std;
 
+typedef struct Sphere{
+	int x;
+	int y;
+	int z;
+	int radius;
+	int xAcc;
+	int yAcc;
+	int zAcc;
+} Sphere;
+
 int main()
 {
 	int testNum;
 	cin >> testNum;
 
 	while(testNum--){
+		int r1, r2;
+		int x, y, z;
+		int xA, yA, zA;
+		cin >> r1 >> r2;
+		cin >> x >> y >> z;
+		cin >> xA >> yA >> zA;
+		Sphere s1 = {x, y, z, r1, xA, yA, zA};
+		cin >> x >> y >> z;
+		cin >> xA >> yA >> zA;
+		Sphere s2 = {x, y, z, r2, xA, yA, zA};
 
 	}
 

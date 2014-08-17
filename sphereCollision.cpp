88ace5df18e75b684dddbb549d09f335d4ccdabe
Jesus Magana <maganazero5@gmail.com>
2014-08-16 19:15:31 -0700

    Made more modular

--- sphereCollision.cpp
+++ sphereCollision.cpp
@@ -3,14 +3,16 @@
 
 using namespace std;
 
-typedef struct Sphere{
+typedef struct Point{
 	int x;
 	int y;
 	int z;
+} Point;
+
+typedef struct Sphere{
+	Point origin;
 	int radius;
-	int xAcc;
-	int yAcc;
-	int zAcc;
+	Point movement;
 } Sphere;
 
 int main()
@@ -19,23 +21,36 @@ int main()
 	cin >> testNum;
 
 	while(testNum--){
+		//getting the input and putting it into spheres
 		int r1, r2;
 		int x, y, z;
 		int xA, yA, zA;
 		cin >> r1 >> r2;
 		cin >> x >> y >> z;
 		cin >> xA >> yA >> zA;
-		Sphere s1 = {x, y, z, r1, xA, yA, zA};
+		Point origin1 = {x,y,z};
+		Point move1 = {xA,yA,zA};
+		Sphere s1 = {origin1, r1, move1};
 		cin >> x >> y >> z;
 		cin >> xA >> yA >> zA;
-		Sphere s2 = {x, y, z, r2, xA, yA, zA};
+		Point origin2 = {x,y,z};
+		Point move2 = {xA,yA,zA};
+		Sphere s2 = {origin2, r2, origin2};
 
-		int dist = sqrt(pow((s1.x - s2.x), 2) + pow((s1.y - s2.y), 2) + pow((s1.z - s2.z), 2));
-		dist -= (s1.radius + s2.radius);
+		//Turning the acceleration of the second one into inverse acceleration of first
+		s1.movement.x -= s2.movement.x;
+		s1.movement.y -= s2.movement.y;
+		s1.movement.z -= s2.movement.z;
+
+		//Check if spheres start together
+		int dist = pow((s1.origin.x - s2.origin.x), 2) + pow((s1.origin.y - s2.origin.y), 2) + pow((s1.origin.z - s2.origin.z), 2);
+		dist -= pow((s1.radius + s2.radius), 2);
 		if(dist <= 0){
-			cout << "NO" << endl;
+			cout << "YES" << endl;
 			continue;
 		}
+
+		
 
 	}
 

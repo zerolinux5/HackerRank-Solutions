6e83c2c90e14dbb36f42a31548be7b8613f7c832
Jesus Magana <maganazero5@gmail.com>
2014-09-04 14:26:01 -0700

    Starting code to see if two words anagrams

---
+++ makeItAnAnagram.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+	string wordOne, wordTwo;
+	cin >> wordOne >> wordTwo;
+
+	cout << wordOne << " " << wordTwo << endl;
+
+	return 0;
+}

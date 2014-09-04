4daf1264351b8076d79eaa3eab10cdac4f17e2bd
Jesus Magana <maganazero5@gmail.com>
2014-09-04 15:08:37 -0700

    Solved using bruteforce via sort and subtract

--- cutTheSticks.cpp
+++ cutTheSticks.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -6,8 +7,29 @@ int main()
 {
 	int numOfSticks;
 	cin >> numOfSticks;
+	int stickArray[numOfSticks];
+	for(int i = 0; i < numOfSticks;i++){
+		cin >> stickArray[i];
+	}
+	int start = 0;
+	int end = numOfSticks;
 
-	cout << numOfSticks;
+	while(numOfSticks != 0){
+		cout << numOfSticks << endl;
+		sort(stickArray, stickArray + end);
+		int cut = stickArray[start];
+
+
+		for(int i = start; i < end;i++){
+			if(stickArray[i] - cut <= 0){
+				start++;
+				numOfSticks--;
+			}
+			stickArray[i] -= cut;
+			//cout << stickArray[i];
+		}
+		//cout << endl;
+	}
 
 	return 0;
 }

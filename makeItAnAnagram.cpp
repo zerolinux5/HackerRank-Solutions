23384f196f1e692068b5a889e0b3b9804c43e73f
Jesus Magana <maganazero5@gmail.com>
2014-09-04 14:31:37 -0700

    Solved using an array to keep track of values

--- makeItAnAnagram.cpp
+++ makeItAnAnagram.cpp
@@ -8,8 +8,30 @@ int main(){
 
 	string wordOne, wordTwo;
 	cin >> wordOne >> wordTwo;
+	int numOfDeletions = 0;
 
-	cout << wordOne << " " << wordTwo << endl;
+	int alphabet[26];
+	for(int i = 0; i < 26;i++){
+		alphabet[i] = 0;
+	}
+
+	for(int i = 0; i < (wordOne.length()); i++){
+		int insertNum = (wordOne[i] - 'a');
+		alphabet[insertNum]--;
+	}
+
+	for(int i = 0; i < (wordTwo.length()); i++){
+		int insertNum = (wordTwo[i] - 'a');
+		alphabet[insertNum]++;
+	}
+
+	for(int i = 0; i < 26; i ++){
+		if(alphabet[i] != 0){
+			numOfDeletions += abs(alphabet[i]);
+		}
+	}
+
+	cout << numOfDeletions << endl;
 
 	return 0;
 }

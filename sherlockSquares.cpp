bc7064aaae744ba59a20c863fadf903b35d7e3ab
Jesus Magana <maganazero5@gmail.com>
2014-09-04 14:51:02 -0700

    Solved via bruteforce

--- sherlockSquares.cpp
+++ sherlockSquares.cpp
@@ -8,7 +8,23 @@ int main()
 	int testNum;
 	cin >> testNum;
 	while(testNum--){
+		int min, max;
+		cin >> min >> max;
+		int numOfSquares = 0;
+		int i = 1;
+		int square = pow(i, 2);
 
+		//Logic
+		while(square <= max){
+			if(square < min){
+			} else {
+				numOfSquares++;
+			}
+			i++;
+			square = pow(i,2);
+		}
+
+		cout << numOfSquares << endl;
 	}
 
 	return 0;

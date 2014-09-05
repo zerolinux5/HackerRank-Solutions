0e04f2b3c8b77645243eb446ee2675e7a34f83ba
Jesus Magana <maganazero5@gmail.com>
2014-09-05 14:00:05 -0700

    Solved via an array and checking each value

--- pangrams.cpp
+++ pangrams.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,8 +7,39 @@ int main()
 {
 	string inputString;
 	getline( cin, inputString);
+	int alphabetTab = 0;
+	int alphabet[26];
+	int flag = 0;
+	for(int i = 0; i < 26; i++){
+		alphabet[i] = 0;
+	}
 
-	cout << inputString << endl;
+	//cout << inputString << endl;
+	for(int i = 0; i < inputString.length(); i++){
+		int insertNum = (tolower(inputString[i]) - 'a');
+		if(insertNum < 0 || insertNum > 25){
+			continue;
+		}
+		if(alphabet[insertNum] == 0){
+			alphabetTab++;
+		}
+		if(alphabetTab >= 26){
+			flag = 1;
+			break;
+		} else {
+			alphabet[insertNum]++;
+		}
+	}
+
+/*	for(int i = 0; i < 26; i++){
+		cout << alphabet[i] << endl;
+	}*/
+
+	if(flag){
+		cout << "pangram" << endl;
+	} else {
+		cout << "not pangram" << endl;
+	}
 
 	return 0;
 }

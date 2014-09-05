717a09c0e5fe5d41e1642d62b4a36c1a94828f7f
Jesus Magana <maganazero5@gmail.com>
2014-09-05 14:29:53 -0700

    works but some cases it wants to opposite number instead of first number

--- palindromeIndex.cpp
+++ palindromeIndex.cpp
@@ -8,6 +8,23 @@ int main()
 	cin >> testNum;
 
 	while(testNum--){
+		string inputString;
+		cin >> inputString;
+		int location = -1;
+
+		for(int i = 0; i < inputString.length()/2;i++){
+			if(inputString[i] != inputString[inputString.length() - (i + 1)]){
+				if(inputString[i+1] == inputString[inputString.length() - (i + 1)]){
+					location = i;
+					break;
+				} else if (inputString[i] == inputString[inputString.length() - (i + 2)]){
+					location = inputString.length() - (i + 1);
+					break;
+				}
+			}
+		}
+
+		cout << location << endl;
 
 	}
 

bbed5fa5460e46e269b2918ac40fa9eb78b04695
Jesus Magana <maganazero5@gmail.com>
2014-09-05 13:30:47 -0700

    Added README to keep tabs of what projects on hold

--- teamAndTopics.cpp
+++ teamAndTopics.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string>
 
 using namespace std;
 
@@ -6,6 +8,34 @@ int main()
 {
 	int people, topics;
 	cin >> people >> topics;
+
+	string peopleTopicArray[500];
+	for(int i = 0; i < people; i++){
+		cin >> peopleTopicArray[i];
+	}
+
+	cout << endl;
+
+	for(int i = 0; i < people; i++){
+		cout << strtol(peopleTopicArray[i],NULL,2) << endl;
+		cout << peopleTopicArray[i] << endl;
+	}
+
+/*	cout << endl;
+	int max = 0;
+	for(int i = 0; i < (people - 1); i++){
+		for(int j = i+1; j< people; j++){
+			int bitOr = peopleTopicArray[i] | peopleTopicArray[j];
+			cout << i << " And " << j << " " << bitOr << endl;
+			if(bitOr > max){
+				max = bitOr;
+			}
+		}
+	}*/
+
+	//cout << max << endl;
+
+
 
 	return 0;
 }

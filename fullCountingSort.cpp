46e2457d8f39d266bb53cb76b74729a77dfb1a18
Jesus Magana <maganazero5@gmail.com>
2014-09-06 18:56:49 -0700

    Read in all the values

--- fullCountingSort.cpp
+++ fullCountingSort.cpp
@@ -7,6 +7,20 @@ int main()
 	int size;
 	cin >> size;
 
+	int numArray[size];
+	string stringArray[size];
+
+	for(int i = 0;i < size; i++){
+		if(i < size/2){
+			cin >> numArray[i];
+			cin >> stringArray[i];
+			stringArray[i] = "-";
+		} else {
+			cin >> numArray[i];
+			cin >> stringArray[i];
+		}
+	}
+
 	return 0;
 }
 

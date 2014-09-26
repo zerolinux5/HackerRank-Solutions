452fede1be10e056a702b9d39a9d343ab4869a06
Jesus Magana <maganazero5@gmail.com>
2014-09-25 20:13:57 -0700

    Solved using recursion

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,17 +2,32 @@
 
 using namespace std;
 
+int binarySearch(int start, int end, int search, int array[])
+{
+	if((end - start) <= 0){
+		return 0;
+	}
+	int mid = (end + start)/2;
+	if(array[mid] == search){
+		return 1;
+	} else if (array[mid] > search){
+		return binarySearch(start,mid,search,array);
+	} else {
+		return binarySearch(mid,end,search,array);
+	}
+}
+
 int main()
 {
-	int size;
-	cin >> size;
+	int size, search;
+	cin >> size >> search;
 	
 	int inArray[size];
 	for(int i = 0; i < size; i++){
 		cin >> inArray[i];
 	}
 
-	for(int i = 0; i < size; i++){
-		cout << inArray[i];
-	}
+	cout << binarySearch(0, size, search, inArray) << endl;
+
+	return 0;
 }

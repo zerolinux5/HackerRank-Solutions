abd7e0ba30d2ed94f1d57d06d2016004bd67bc1b
Jesus Magana <maganazero5@gmail.com>
2014-09-27 17:55:48 -0700

    Solved merge sort via recursion

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+int* mergeArray(int arrayA[], int arrayB[],int sizeA, int sizeB)
+{
+	int finalLength = sizeA + sizeB;
+	int *finalArray = new int[finalLength];
+
+	int pointA = 0, pointB = 0;
+	for(int i = 0; i < finalLength; i++){
+		if ( pointA >= sizeA) {
+			finalArray[i] = arrayB[pointB];
+			pointB++;
+		} else if (pointB >= sizeB) {
+			finalArray[i] = arrayA[pointA];
+			pointA++;
+		} else if(arrayA[pointA] > arrayB[pointB]){
+			finalArray[i] = arrayB[pointB];
+			pointB++;
+		} else {
+			finalArray[i] = arrayA[pointA];
+			pointA++;
+		}
+	}
+
+	return finalArray;
+}
+
+int* mergeSort(int start, int size, int* mArray)
+{
+	if(size-start <= 1){
+		int *single = new int[size-start];
+		int j = 0;
+		for(int i = start; i < size; i++){
+			single[j] = mArray[i];
+			j++;
+		}
+		return single;
+	} else {
+		int mid = (size+start)/2;
+		int* left = mergeSort(start, mid, mArray);
+		int* right = mergeSort(mid,size, mArray);
+		
+		int* returnArray = mergeArray(left, right, (mid - start), (size - mid));
+
+		cout << "Returning: ";
+		for(int i = 0; i < size;i++){
+			cout << returnArray[i] << " ";
+		}
+		cout << endl;
+		return returnArray;
+	}
+}
 
 int main()
 {
@@ -13,8 +65,11 @@ int main()
 		cin >> mArray[i];
 	}
 
+	int* sortedArray = mergeSort(0, size, mArray);
+	
+	//Debug only
 	for(int i = 0; i < size; i++){
-		cout << mArray[i] << endl;
+		cout << sortedArray[i] << endl;
 	}
 
 	return 0;

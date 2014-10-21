b7b7cc7072c1ea956dc2dcfd5a85743495712afd
Jesus Magana <maganazero5@gmail.com>
2014-10-21 14:12:19 -0700

    Fixed input/output typo

--- neighborMatrix.cpp
+++ neighborMatrix.cpp
@@ -22,29 +22,80 @@ int main()
 		for(int j = 0; j < sizeN;j++){
 			//top left corner
 			if(i == 0 && j == 0){
-				int neighborInt = outputArray[i+1][j];
-				neighborInt += outputArray[i][j+1];
-				neighborInt += outputArray[i+1][j+1];
+				int neighborInt = inputArray[i+1][j];
+				neighborInt += inputArray[i][j+1];
+				neighborInt += inputArray[i+1][j+1];
 				outputArray[i][j] = neighborInt;
 				//top right corner
-			} else if (i == sizeM - 1 && j == 0){
-				int neighborInt = outputArray[i][j-1];
-				neighborInt += outputArray[i+1][j];
-				neighborInt += outputArray[i-1][j-1];
+			} else if ((i == sizeM - 1) && (j == 0)){
+				int neighborInt = inputArray[i][j-1];
+				neighborInt += inputArray[i+1][j];
+				neighborInt += inputArray[i-1][j-1];
 				outputArray[i][j] = neighborInt;
 				//bottom left corner
-			} else if (i == 0 && j == sizeN - 1) {
-				int neighborInt = outputArray[i-1][j];
-				neighborInt += outputArray[i][j+1];
-				neighborInt += outputArray[i-1][j+1];
+			} else if (i == 0 && (j == sizeN - 1)) {
+				int neighborInt = inputArray[i-1][j];
+				neighborInt += inputArray[i][j+1];
+				neighborInt += inputArray[i-1][j+1];
 				outputArray[i][j] = neighborInt;
 				//bottom right corner
-			} else if (i == sizeM - 1 && j == sizeN - 1){
-				int neighborInt = outputArray[i][j-1];
-				neighborInt += outputArray[i-1][j];
-				neighborInt += outputArray[i-1][j-1];
+			} else if ((i == sizeM - 1) && (j == sizeN - 1)){
+				int neighborInt = inputArray[i][j-1];
+				neighborInt += inputArray[i-1][j];
+				neighborInt += inputArray[i-1][j-1];
 				outputArray[i][j] = neighborInt;
+				//top row
+			} else if (i == 0){
+				int neighborInt = inputArray[i][j-1];
+				neighborInt = inputArray[i][j+1];
+				neighborInt += inputArray[i+1][j-1];
+				neighborInt += inputArray[i+1][j+1];
+				neighborInt += inputArray[i+1][j];
+				outputArray[i][j] = neighborInt;
+				//bottom row
+			} else if (i ==  sizeM - 1){
+				int neighborInt = inputArray[i][j-1];
+				neighborInt = inputArray[i][j+1];
+				neighborInt += inputArray[i-1][j-1];
+				neighborInt += inputArray[i-1][j+1];
+				neighborInt += inputArray[i-1][j];
+				outputArray[i][j] = neighborInt;
+				//left column
+			} else if (j == 0){
+				int neighborInt = inputArray[i][j+1];
+				neighborInt += inputArray[i-1][j];
+				neighborInt += inputArray[i-1][j+1];
+				neighborInt += inputArray[i+1][j];
+				neighborInt += inputArray[i+1][j+1];
+				outputArray[i][j] = neighborInt;
+				//right column
+			} else if (j == sizeN - 1){
+				int neighborInt = inputArray[i][j-1];
+				neighborInt += inputArray[i-1][j];
+				neighborInt += inputArray[i-1][j-1];
+				neighborInt += inputArray[i+1][j];
+				neighborInt += inputArray[i+1][j-1];
+				outputArray[i][j] = neighborInt;
+				//anything else
+			} else {
+				int neighborInt = inputArray[i][j-1];
+				neighborInt = inputArray[i][j+1];
+				neighborInt += inputArray[i-1][j];
+				neighborInt += inputArray[i-1][j-1];
+				neighborInt += inputArray[i-1][j+1];
+				neighborInt += inputArray[i+1][j];
+				neighborInt += inputArray[i+1][j-1];
+				neighborInt += inputArray[i+1][j+1];
+				outputArray[i][j] = neighborInt;
+
 			}
 		}
+	}
+
+	for(int i = 0; i < sizeM; i++){
+		for(int j = 0; j < sizeN; j++){
+			cout << outputArray[i][j] << " ";
+		}
+		cout << endl;
 	}
 }

f49ed8c1b83b5281f36de66997f7aa10081f9e98
Jesus Magana <maganazero5@gmail.com>
2014-10-21 14:03:09 -0700

    Filled in the corner casses literally

--- neighborMatrix.cpp
+++ neighborMatrix.cpp
@@ -8,10 +8,43 @@ int main()
 	cin >> sizeM >> sizeN;
 
 	int inputArray[sizeM][sizeN];
+	int outputArray[sizeM][sizeN];
 
+	//Read in the multidimentional matrix
 	for(int i = 0; i < sizeM;i++){
 		for(int j = 0; j < sizeN; j++){
 			cin >> inputArray[i][j];
+		}
+	}
+
+	//brute force attempt
+	for(int i = 0; i < sizeM; i++){
+		for(int j = 0; j < sizeN;j++){
+			//top left corner
+			if(i == 0 && j == 0){
+				int neighborInt = outputArray[i+1][j];
+				neighborInt += outputArray[i][j+1];
+				neighborInt += outputArray[i+1][j+1];
+				outputArray[i][j] = neighborInt;
+				//top right corner
+			} else if (i == sizeM - 1 && j == 0){
+				int neighborInt = outputArray[i][j-1];
+				neighborInt += outputArray[i+1][j];
+				neighborInt += outputArray[i-1][j-1];
+				outputArray[i][j] = neighborInt;
+				//bottom left corner
+			} else if (i == 0 && j == sizeN - 1) {
+				int neighborInt = outputArray[i-1][j];
+				neighborInt += outputArray[i][j+1];
+				neighborInt += outputArray[i-1][j+1];
+				outputArray[i][j] = neighborInt;
+				//bottom right corner
+			} else if (i == sizeM - 1 && j == sizeN - 1){
+				int neighborInt = outputArray[i][j-1];
+				neighborInt += outputArray[i-1][j];
+				neighborInt += outputArray[i-1][j-1];
+				outputArray[i][j] = neighborInt;
+			}
 		}
 	}
 }

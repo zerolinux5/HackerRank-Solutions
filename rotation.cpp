fd127013db01d1e20cae4e79158acfce801b8532
Jesus Magana <maganazero5@gmail.com>
2014-10-22 22:01:00 -0700

    inplace rotation works

--- rotation.cpp
+++ rotation.cpp
@@ -8,14 +8,28 @@ int main()
 	cin >> size >> turns >> indexes;
 
 	int circularArray[size];
+	int counter = 0;
 
 	for(int i = 0; i < size; i++){
 		cin >> circularArray[i];
 	}
 
+	int oldIndex = 0;
+	int oldValue = circularArray[0];
+	while(counter < size){
+		int newlocation = oldIndex + turns;
+		newlocation %= size;
+		int tempValue = circularArray[newlocation];
+		circularArray[newlocation] = oldValue;
+		oldValue = tempValue;
+		oldIndex = newlocation;
+		counter++; 
+	}
+
+	///* Debugg to check array
 	for(int i = 0; i < size; i++){
 		cout << circularArray[i] << endl;
-	}
+	}//*/
 
 	return 0;
 
